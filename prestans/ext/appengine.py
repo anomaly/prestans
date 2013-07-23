dcf664ce90604bec37f8a251ba1dd7b7d9905b5f
Devraj Mukherjee <devraj@gmail.com>
2013-07-23 15:44:18 +1000

    Ported AppEngine customisations

--- prestans/ext/appengine.py
+++ prestans/ext/appengine.py
@@ -29,3 +29,79 @@
 #  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+
+__all__ = ['AppEngineAuthContextProvider', 'AppEngineCacheProvider']
+
+## @package prestans.appengine Providers Google AppEngine specific implementation of providers
+#
+# appengine is not imported as part of "all", this has to be explicity imported by the user.
+#
+
+from prestans import auth
+from prestans import cache
+from prestans import throttle
+
+from google.appengine.api import oauth
+from google.appengine.api import users
+from google.appengine.api import memcache
+
+import os
+
+_IS_DEVELOPMENT_SERVER = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
+
+#:
+#: Provides Authentication Context for Google's AppEngine Environment
+#:
+#:
+#: auth.AuthContextProvider implementation for Google AppEngine, uses google.appengine.api.users
+#: to determine logged in users and return a reference to the current user.
+#:
+#: Refer to decorators in handlers package for more information.
+#:
+class AppEngineAuthContextProvider(auth.AuthContextProvider):
+    
+    #:
+    #: Overriden is_authenticated_user for Google AppEngine
+    #: Uses google.appengine.api.users method get_current_user to check if a user is logged in 
+    #:
+    def is_authenticated_user(self):
+        return users.get_current_user() is not None
+      
+    #:  
+    #: Overriden get_current_user for Google AppEngine
+    #: Checks for oauth capable request first, if this fails fall back to standard users API
+    #:
+    def get_current_user(self):
+
+        if _IS_DEVELOPMENT_SERVER:
+            return users.get_current_user()
+        else:
+            try:
+                user = oauth.get_current_user()
+            except oauth.OAuthRequestError, exp:
+                user = users.get_current_user()
+            return user
+  
+#:      
+#: Provides a wrapper on AppEngine's memcache implementation 
+#:
+class AppEngineMemcacheProvider(cache.CacheProvider):
+    
+    def __init__(self, key_provider=None):
+        self.key_provider = key_provider
+    
+    def get(self, key):
+        memcache.get(key)       
+    
+    def set(self, key, value, expiry=60, namespace=None):
+        memcache.set(key, value, expiry)
+        
+    def add(self, key, value, expiry=60, namespace=None):
+        memcache.add(key, value, expiry, namespace)
+        
+    def delete(self, key, seconds=0, namespace=None):
+        memcache.delete(key, seconds, namespace)
+        
+    def flush_all(self):
+        memcache.flush_all()
+
