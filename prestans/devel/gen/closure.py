57d66ddb6e26a5a8375ac2f461808e1fc6aae737
Bradley Mclain <brad@mclain.com.au>
2013-08-12 11:15:57 +1000

    renamed test script

--- prestans/devel/gen/closure.py
+++ prestans/devel/gen/closure.py
@@ -10,6 +10,28 @@ class AttributeMetaData(object):
         self._name = name
         self._blueprint = blueprint
 
+        self._type = blueprint['type']
+        if self._type == 'string':
+            self._required = blueprint['constraints']['required']
+        elif self._type == 'integer':
+            self._required = blueprint['constraints']['required']
+        elif self._type == 'float':
+            self._required = blueprint['constraints']['required']
+        elif self._type == 'boolean':
+            self._required = blueprint['constraints']['required']
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def required(self):
+        return self._required
+
 class Model(object):
 
     def __init__(self, model_file):
@@ -20,13 +42,19 @@ class Model(object):
         inspector = prestans.devel.gen.Inspector(model_file=self._model_file)
         blueprints = inspector.inspect()
 
-        attributes = list()
         for model_blueprint in blueprints:
+
+            attributes = list()
 
             for field_name, field_blueprint in model_blueprint['fields'].iteritems():
 
                 attribute = AttributeMetaData(name=field_name, blueprint=field_blueprint)
-                print attribute
+                attributes.append(attribute)
+
+            #write out template
+            
+
+
 
         return 0
 
