0a005f069cc12dbcd38d60a1ee4404e46fc510ed
Devraj Mukherjee <devraj@gmail.com>
2013-08-27 14:13:51 +1000

    Updated request handler reference

--- prestans/demo/app/pdemo/rest/handlers/__init__.py
+++ prestans/demo/app/pdemo/rest/handlers/__init__.py
@@ -31,7 +31,7 @@
 #  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-import prestans.handlers
+import prestans.rest
 
-class Base(prestans.handlers.RESTRequestHandler):
+class Base(prestans.rest.RequestHandler):
 	pass
