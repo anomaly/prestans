f86ddacc83f811946b8b045ee3797247ca86699d
Bradley Mclain <brad@mclain.com.au>
2013-08-27 11:19:15 +1000

    removed old sample models

--- prestans/demo/app/pdemo/rest/models.py
+++ prestans/demo/app/pdemo/rest/models.py
@@ -33,60 +33,54 @@
 
 from prestans import types
 
-class StringSample(types.Model):
-	string_required = types.String(required=True)
-	string_not_required = types.String(required=False)
-	string_default = types.String(default="Hello World")
-	string_min_length = types.String(min_length=1)
-	string_max_length = types.String(max_length=5)
-	string_choices = types.String(choices=["A", "B", "C"])
-	string_format = types.String(format="^[0-9]+[a-z]{3}$")
+class Player(types.Model):
+	player_id = types.Integer(required=False)
+	first_name = types.String(required=True)
+	last_name = types.String(required=True)
+	#height = 
+	weight = types.Integer(minimum=0, required=True)
+	dob = types.Date(required=True)
 
-class IntegerSample(types.Model):
-	integer_required = types.Integer(required=True)
-	integer_not_required = types.Integer(required=False)
-	integer_default = types.Integer(default=6)
-	integer_minimum = types.Integer(minimum=1)
-	integer_maximum = types.Integer(maximum=5)
-	integer_choices = types.Integer(choices=[1, 2, 3])
+class PlayerMatchStats(types.Model):
 
-class FloatSample(types.Model):
-	float_required = types.Float(required=True)
-	float_not_required = types.Float(required=False)
-	float_default = types.Float(default=6.0)
-	float_minimum = types.Float(minimum=1.0)
-	float_maximum = types.Float(maximum=5.0)
-	float_choices = types.Float(choices=[1.1, 2.2, 3.3])
+	match_id = types.Integer(required=True)
+	player_id = types.Integer(required=True)
 
-class BooleanSample(types.Model):
-	boolean_required = types.Boolean(required=True)
-	boolean_not_required = types.Boolean(required=False)
-	boolean_default = types.Boolean(default=True)
+	minutes = types.Integer(minimum=0, default=0) #mintues played
 
-class DateTime(types.Model):
-	datetime_required = types.DateTime(required=True)
-	datetime_not_required = types.DateTime(required=False)
-	datetime_default_now = types.DateTime(default=types.DateTime.CONSTANT.NOW)
-	datetime_default_string = types.DateTime(default="2013-01-01 12:01:34")
+	fgm = types.Integer(minimum=0, default=0) #field goals made
+	fga = types.Integer(minimum=0, default=0) #field goals attempted
 
-class Date(types.Model):
-	date_required = types.Date(required=True)
-	date_not_required = types.Date(required=False)
-	date_default_today = types.Date(default=types.Date.CONSTANT.TODAY)
-	date_default_string = types.Date(default="2013-01-02")
+	tpm = types.Integer(minimum=0, default=0) #three pointers made
+	tpa = types.Integer(minimum=0, default=0) #three pointers attempted
 
-class Time(types.Model):
-	time_required = types.Date(required=True)
-	time_not_required = types.Date(required=False)
-	time_default_now = types.Date(default=types.Time.CONSTANT.NOW)
+	ftm = types.Integer(minimum=0, default=0) #free throws made
+	fta = types.Integer(minimum=0, default=0) #free throws attempted
 
-class ModelSample(types.Model):
-	integer_sample = IntegerSample()
-	string_sample = StringSample()
-	string_title = types.String(required=True)
+	reb = types.Integer(minimum=0, default=0) #rebounds
+	ast = types.Integer(minimum=0, default=0) #assists
+	blk = types.Integer(minimum=0, default=0) #blocks
+	stl = types.Integer(minimum=0, default=0) #steals
+	pf = types.Integer(minimum=0, default=0) #personal fouls
+	to = types.Integer(minimum=0, default=0) #turnovers
+	pts = types.Integer(minimum=0, default=0) #points
 
-class ArraySample(types.Model):
-	model_array = types.Array(element_template=IntegerSample())
-	integer_array = types.Array(element_template=types.Integer())
-	string_array = types.Array(element_template=types.String())
-	string_title = types.String(required=True)
+class Match(types.Model):
+	match_id = types.Integer(required=True)
+	season_id = types.Integer(required=True)
+
+	venue = types.String(required=True)
+
+	player_stats = types.Array(element_template=PlayerMatchStats())
+
+class Season(types.Model):
+	season_id = types.Integer(required=True)
+
+class Team(types.Model):
+	team_id = types.String(required=True)
+	city = types.String(required=True)
+	name = types.String(required=True)
+
+
+
+
