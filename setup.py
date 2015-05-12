f84a89b67ef1fa07a6c6121fe5dd46c36e8157ae
Dev Mukherjee <devraj@gmail.com>
2015-05-12 13:04:54 +1000

    Bumps up version number to 2.0.9

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@
 from setuptools import setup, find_packages
 
 setup(name='prestans',
-      version='2.0.8',
+      version='2.0.9',
       description='A WSGI compliant REST micro-framework',
       url='https://github.com/anomaly/prestans.git',
       long_description="""
@@ -55,7 +55,7 @@ setup(name='prestans',
       - Pluggable integration to authentication
 
       """,
-      download_url='https://github.com/anomaly/prestans/archive/2.0.8.tar.gz',
+      download_url='https://github.com/anomaly/prestans/archive/2.0.9.tar.gz',
       license='New BSD',
       author='Anomaly Software',
       author_email='support@anomaly.net.au',
