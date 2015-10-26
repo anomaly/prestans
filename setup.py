f4d1735f888719a18436b66628d4b2d9aec47ed9
Bradley Mclain <brad@mclain.com.au>
2015-10-27 09:47:08 +1100

    fix for #96

--- setup.py
+++ setup.py
@@ -35,47 +35,48 @@ from setuptools import setup, find_packages
 from prestans import __version__
 
 setup(name='prestans',
-      version=__version__,
-      description='A WSGI compliant REST micro-framework',
-      url='https://github.com/anomaly/prestans.git',
-      long_description="""
-      Prestans is a REST micro-framework built right on top of WSGI, designed to perform and co-exists with other middleware and frameworks that you employ. 
+    version=__version__,
+    description='A WSGI compliant REST micro-framework',
+    url='https://github.com/anomaly/prestans.git',
+    long_description="""
+    Prestans is a REST micro-framework built right on top of WSGI, designed to perform and co-exists with other middleware and frameworks that you employ. 
 
-      Our target audience are developers building large REST backends for use with pure Ajax (using a framework like Google Closure) or mobile applications. 
+    Our target audience are developers building large REST backends for use with pure Ajax (using a framework like Google Closure) or mobile applications. 
 
-      Prestans is designed for you to "take as much or as little" as you like.
+    Prestans is designed for you to "take as much or as little" as you like.
 
-      Features:
+    Features:
 
-      - Built right on top of WSGI, designed ground up to cooperate with other frameworks
-      - Strong representative of REST philosophies leveraging HTTP headers and verbs
-      - Support for multiple dialects, including formalised patterns for binary content 
-      - Handlers maps HTTP verbs to implemented class methods, complimented  with custom 
-        request parser and response writer (built on top of WebOb)
-      - Unforgivingly strict parsing of requests and responses to ensure data integrity
-      - Helper methods to ease transformation of persistent objects to REST responses
-      - Pluggable integration to authentication
+    - Built right on top of WSGI, designed ground up to cooperate with other frameworks
+    - Strong representative of REST philosophies leveraging HTTP headers and verbs
+    - Support for multiple dialects, including formalised patterns for binary content 
+    - Handlers maps HTTP verbs to implemented class methods, complimented  with custom 
+    request parser and response writer (built on top of WebOb)
+    - Unforgivingly strict parsing of requests and responses to ensure data integrity
+    - Helper methods to ease transformation of persistent objects to REST responses
+    - Pluggable integration to authentication
 
-      """,
-      download_url='https://github.com/anomaly/prestans/archive/' + __version__ + '.tar.gz',
-      license='New BSD',
-      author='Anomaly Software',
-      author_email='support@anomaly.net.au',
-      maintainer='Anomaly Software',
-      maintainer_email='support@anomaly.net.au',
-      platforms=['any'],
-      scripts=['prestans/bin/pride'],
-      packages=find_packages(),
-      classifiers=[
-            'Environment :: Web Environment',
-            'Intended Audience :: Developers',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Internet :: WWW/HTTP',
-            'Topic :: Internet :: WWW/HTTP :: WSGI',
-            'Topic :: Software Development :: Libraries :: Application Frameworks',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
-      install_requires=['WebOb==1.4',],
-     )
+    """,
+    download_url='https://github.com/anomaly/prestans/archive/' + __version__ + '.tar.gz',
+    license='New BSD',
+    author='Anomaly Software',
+    author_email='support@anomaly.net.au',
+    maintainer='Anomaly Software',
+    maintainer_email='support@anomaly.net.au',
+    platforms=['any'],
+    scripts=['prestans/bin/pride'],
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=['WebOb==1.4',],
+    include_package_data = True
+)
