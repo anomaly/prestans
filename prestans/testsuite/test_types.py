9c75398a5b7acdcb197e02efa84d784ee51669ae
Bradley Mclain <brad@mclain.com.au>
2016-06-02 14:34:19 +1000

    added unit tests for integer #87, fix for #108

--- prestans/testsuite/test_types.py
+++ prestans/testsuite/test_types.py
@@ -38,10 +38,9 @@ import prestans.exception
 class StringTypeUnitTest(unittest.TestCase):
 
     def setUp(self):
-        
         self.required = prestans.types.String(required=True)
         self.not_required = prestans.types.String(required=False)
-        self.type = prestans.types.String()
+        self.types = prestans.types.String()
         self.default = prestans.types.String(default="orange")
         self.length = prestans.types.String(min_length=5, max_length=7)
         self.format = prestans.types.String(format="[0-9]{2}[a-z]{5}[0-9]{3}")
@@ -56,11 +55,11 @@ class StringTypeUnitTest(unittest.TestCase):
         self.assertEqual(self.not_required.validate(""), "")
         self.assertEqual(self.not_required.validate(None), None)
 
-    def test_type(self):
-        self.assertEqual(self.type.validate("orange"), "orange")
-        self.assertEqual(self.type.validate(1), "1")
-        self.assertEqual(self.type.validate(1.0), "1.0")
-        self.assertEqual(self.type.validate(True), "True")
+    def test_types(self):
+        self.assertEqual(self.types.validate("orange"), "orange")
+        self.assertEqual(self.types.validate(1), "1")
+        self.assertEqual(self.types.validate(1.0), "1.0")
+        self.assertEqual(self.types.validate(True), "True")
 
     def test_default(self):
         self.assertEqual(self.default.validate(None), "orange")
@@ -96,28 +95,72 @@ class StringTypeUnitTest(unittest.TestCase):
 class IntegerTypeUnitTest(unittest.TestCase):
 
     def setUp(self):
-        
-        self._integer = prestans.types.Integer(
-            default=1,
-            minimum=1,
-            maximum=5
-        )
+        self.types = prestans.types.Integer()
+        self.required = prestans.types.Integer(required=True)
+        self.not_required = prestans.types.Integer(required=False)
+        self.default = prestans.types.Integer(default=6)
+        self.positive_range = prestans.types.Integer(minimum=1, maximum=5)
+        self.negative_range = prestans.types.Integer(minimum=-5, maximum=-1)
+        self.zero_min_range = prestans.types.Integer(minimum=0)
+        self.zero_max_range = prestans.types.Integer(maximum=0)
+        self.choices = prestans.types.Integer(choices=[1,5])
 
-        self._choices = prestans.types.Integer(
-            choices=[1,5]
-        )
+    def test_types(self):
+        self.assertRaises(prestans.exception.ParseFailedError, self.types.validate, "string")
+
+    def test_required(self):
+        self.assertRaises(prestans.exception.RequiredAttributeError, self.required.validate, None)
+        self.assertEqual(self.required.validate(1), 1)
+
+        self.assertEqual(self.not_required.validate(1), 1)
+        self.assertEqual(self.not_required.validate(None), None)
 
     def test_default(self):
-        self.assertEqual(self._integer.validate(None), 1)
+        self.assertEqual(self.default.validate(None), 6)
+        self.assertEqual(self.default.validate(5), 5)
 
     def test_minimum(self):
-        self.assertRaises(prestans.exception.LessThanMinimumError, self._integer.validate, -1)
+        self.assertRaises(prestans.exception.LessThanMinimumError, self.positive_range.validate, -2)
+        self.assertRaises(prestans.exception.LessThanMinimumError, self.positive_range.validate, -1)
+        self.assertRaises(prestans.exception.LessThanMinimumError, self.positive_range.validate, 0)
+        self.assertEqual(self.positive_range.validate(1), 1)
+        self.assertEqual(self.positive_range.validate(2), 2)
+        self.assertEqual(self.positive_range.validate(3), 3)
+
+        self.assertRaises(prestans.exception.LessThanMinimumError, self.negative_range.validate, -8)
+        self.assertRaises(prestans.exception.LessThanMinimumError, self.negative_range.validate, -7)
+        self.assertRaises(prestans.exception.LessThanMinimumError, self.negative_range.validate, -6)
+        self.assertEqual(self.negative_range.validate(-5), -5)
+        self.assertEqual(self.negative_range.validate(-4), -4)
+        self.assertEqual(self.negative_range.validate(-3), -3)
+
+        self.assertRaises(prestans.exception.LessThanMinimumError, self.zero_min_range.validate, -1)
+        self.assertEqual(self.zero_min_range.validate(0), 0)
 
     def text_maximum(self):
-        self.assertRaises(prestans.exception.MoreThanMaximumError, self._integer.validate, 6)
+        self.assertEqual(self.positive_range.validate(0), 0)
+        self.assertEqual(self.positive_range.validate(3), 3)
+        self.assertEqual(self.positive_range.validate(4), 4)
+        self.assertEqual(self.positive_range.validate(5), 5)
+        self.assertRaises(prestans.exception.MoreThanMaximumError, self.positive_range.validate, 6)
+        self.assertRaises(prestans.exception.MoreThanMaximumError, self.positive_range.validate, 7)
+        self.assertRaises(prestans.exception.MoreThanMaximumError, self.positive_range.validate, 8)
+
+        self.assertEqual(self.negative_range.validate(-3), -3)
+        self.assertEqual(self.negative_range.validate(-2), -2)
+        self.assertEqual(self.negative_range.validate(-1), -1)
+        self.assertRaises(prestans.exception.MoreThanMaximumError, self.negative_range.validate, 0)
+        self.assertRaises(prestans.exception.MoreThanMaximumError, self.negative_range.validate, 1)
+        self.assertRaises(prestans.exception.MoreThanMaximumError, self.negative_range.validate, 2)
+
+        self.assertEqual(self.zero_min_range.validate(-1), -1)
+        self.assertRaises(prestans.exception.MoreThanMaximumError, self.zero_min_range.validate, 1)
 
     def test_choices(self):
-        self.assertRaises(prestans.exception.InvalidChoiceError, self._choices.validate, 3)
+        self.assertRaises(prestans.exception.InvalidChoiceError, self.choices.validate, 3)
+        self.assertRaises(prestans.exception.InvalidChoiceError, self.choices.validate, 4)
+        self.assertEqual(self.choices.validate(1), 1)
+        self.assertEqual(self.choices.validate(5), 5)
 
     def tearDown(self):
         pass
