d90314a9f0e82ca6298f85672de4f091b7097a29
Bradley Mclain <brad@mclain.com.au>
2016-11-07 11:47:29 +1100

    bumps version, potential fix for #143

--- prestans/__init__.py
+++ prestans/__init__.py
@@ -32,5 +32,5 @@
 
 __all__ = ['http', 'types', 'rest', 'parser', 'serializer', 'deserializer', 'provider', 'ext', 'exception']
 
-__version_info__ = (2, 1, 1)
+__version_info__ = (2, 1, 2)
 __version__ = '.'.join(str(v) for v in __version_info__)
