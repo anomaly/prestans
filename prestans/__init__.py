1a349e0fda4202aeb21cc98ede528e22f7061429
Bradley Mclain <brad@mclain.com.au>
2018-02-07 16:02:56 +1100

    bumps version number for 2.2.0

--- prestans/__init__.py
+++ prestans/__init__.py
@@ -32,5 +32,5 @@
 
 __all__ = ['http', 'types', 'rest', 'parser', 'serializer', 'deserializer', 'provider', 'ext', 'exception']
 
-__version_info__ = (2, 1, 6)
+__version_info__ = (2, 2, 0)
 __version__ = '.'.join(str(v) for v in __version_info__)
